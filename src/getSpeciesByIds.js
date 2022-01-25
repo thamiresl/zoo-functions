29e2a87d7263189049a5fdf1632f1b4bdbcc8e5b
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-25 15:46:17 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((element) => ids.includes(element.id));
 }
 
 module.exports = getSpeciesByIds;
