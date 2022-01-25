e72907acd1a10ec960c586edc20186aa1bb485e6
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-25 18:09:48 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animais = species.find((element) => element.name === animal);
+  return animais.residents.every((old) => old.age >= age);
 }
-
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,5 +4,5 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   return species.filter((element) => ids.includes(element.id));
 }
-
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
