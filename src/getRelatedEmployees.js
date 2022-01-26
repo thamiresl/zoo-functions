dffafcee6499a574aed9ac79efcc8730396b43ea
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-26 11:16:03 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managersList = [stephanieId, olaId, burlId];
+
 function isManager(id) {
-  // seu código aqui
+  const manager = data.employees.find((element) => element.id === id);
+  return managersList.some((idManager) => manager.id === idManager);
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const employee = employees
+      .filter(({ managers }) => managers.some((element) => element === managerId));
+    return employee.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = { isManager, getRelatedEmployees };
