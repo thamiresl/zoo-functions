54452e89f9bd304402a9bd13c1394f722b9050da
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-26 19:16:05 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const result = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((visitAge) => {
+    if (visitAge.age < 18) result.child += 1;
+    else if (visitAge.age < 50) result.adult += 1;
+    else result.senior += 1;
+  });
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+
+  const pay = countEntrants(entrants);
+  let ingresso = 0;
+  ingresso += pay.senior * 24.99;
+  ingresso += pay.adult * 49.99;
+  ingresso += pay.child * 20.99;
+  return ingresso;
 }
 
 module.exports = { calculateEntry, countEntrants };
