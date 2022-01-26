b27d959b6040025a6fac6857bb38be66a1f38928
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-26 15:36:59 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  
+  if (!animal) {
+    return data.species.reduce((accumulator, value) => {
+      const nameObj = value.name;
+      const valueObj = value.residents.length;
+      accumulator[nameObj] = valueObj;
+      return accumulator;
+    }, {});
+  }
+  const foundSpecie = data.species.find((specie) => specie.name.includes(animal.specie));
+  if (!animal.sex) {
+    return foundSpecie.residents.length;
+  }
+  return foundSpecie.residents.filter((resident) => resident.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
+// ajuda monitoria Roberval Filho
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -18,6 +18,4 @@ function getRelatedEmployees(managerId) {
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
-console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = { isManager, getRelatedEmployees };
