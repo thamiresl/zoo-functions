bb4682c9b2abdca429acf7f71bd7aa287e3ba423
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-26 21:53:39 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((element) => element.id === id);
+  const animals = employee.responsibleFor[0];
+  const animal = data.species.find((specie) => specie.id === animals);
+
+  const old = animal.residents.reduce((accumulator, currentValue) => {
+    if (currentValue.age > accumulator.age) return currentValue;
+    return accumulator;
+  });
+  return Object.values(old);
 }
 
 module.exports = getOldestFromFirstSpecies;
